86020deb455efdc4f28a8bd23dcac281a49e86a5
Wlandimir Gomes <wlandimirgomes25@gmail.com>
2024-12-03 11:49:32 -0300

    Delete gasolina.py

--- gasolina.py
+++
@@ -1,9 +0,0 @@
-
-  import pandas as pd
-  gasolina_df = pd.read_csv("gasolina.csv", sep=",")
-  gasolina_df.head()
-  import seaborn as sns
-  import matplotlib.pyplot as plt
-  sns.lineplot(data= gasolina_df, x = 'dia', y = 'venda')
-  plt.savefig('gasolina.png')
-  
